b0f6e282ee774fcf4ccacd2a435eff9534f27067
Taras Fedorov <taras.fedorov@thomsonreuters.com>
2018-10-22 18:18:02 +0300

    Some refactoring/cleaning/ proposition

--- src/main/scala/com/epam/spark/Main.scala
+++ src/main/scala/com/epam/spark/Main.scala
@@ -1,39 +1,51 @@
 package com.epam.spark
 
+import java.util.Date
+
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 /**
   * start point*/
-object Main extends App {
+object Main extends App with Engine {
 
   println(args.mkString("\n"))
 
   //resolve input argument
-  val (master: String, file: String) = args.toList match {
-    case firstArg :: secondArg :: Nil => (firstArg, secondArg)
-    case firstArg :: Nil => (firstArg, "src\\main\\scala\\resourses\\countries_of_the_world.csv")
-    case Nil => ("local", "src\\main\\scala\\resourses\\countries_of_the_world.csv")
+  val (master: String, file: String, saveFile: String) = args.toList match {
+    case firstArg :: secondArg :: thirdArg :: Nil => (firstArg, secondArg, thirdArg)
+    case firstArg :: Nil => (firstArg, "src/main/resourses/countries_of_the_world.csv", "./" + new Date().getTime)
+    case Nil => ("local", "src/main/resourses/countries_of_the_world.csv", "./" + new Date().getTime)
   }
 
   private val sparkContext: SparkContext = SparkSession
     .builder()
     .appName("SparkSessionZipsExample")
-    .master("local")
+    .master(master)
     .getOrCreate().sparkContext
 
-  private val inputFile: RDD[String] = sparkContext.textFile(file)
+  private val inputRDD: RDD[String] = sparkContext.textFile(file)
+
+  private val noHeaderRDD: RDD[String] = inputRDD.filter(!isHeaderCsv(_))
 
   //get region, population and density from csv file
-  private val csvFile: RDD[(String, Long, Double)] = Engine.getRDDRegionIPopultionIpopDensity(inputFile)
+  private val parsedRDD: RDD[(String, Long, Double)] = noHeaderRDD.map(getCaseRegionIPopultionIpopDensity)
 
   //Show to console sum of population
-  println("Population total: " + Engine.getPopulationSum(csvFile))
+  //println("Population total: " + getPopulationSum(parsedRDD))
 
   //Show Population Density at region
   println("Regional Population Density:")
-  Engine.getPopulationDensityAtRegions(csvFile).foreach((x: (String, Double)) => println("* " + x))
+  val regionsRDD: RDD[(String, Double)] = parsedRDD.map { case (x1, x2, x3) => (clearKey(x1), x3)
+  }.reduceByKey(_ + _)
 
-  Thread.sleep(10000)
+  val resultStringRDD: RDD[String] = regionsRDD.map(_.toString)
+
+  //resultStringRDD.saveAsTextFile(saveFile)
+  resultStringRDD.collect().foreach(println)
+
+  // while (true) {
+  //   Thread.sleep(10000)
+  // }
 }
--- src/test/scala/com/epam/spark/TestEngine.scala
+++ src/test/scala/com/epam/spark/TestEngine.scala
@@ -2,10 +2,6 @@ package scala.com.epam.spark
 
 
 import com.epam.spark.Engine
-import com.epam.spark.Engine.clearKey
-import org.apache.spark.SparkContext
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.SparkSession
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -14,84 +10,36 @@ class TestEngine {
   @Test
   def clearKeyTest(): Unit = {
     val input = "ASIA (EX. NEAR EAST)         "
+
+    val actualResult = new Engine() {}.clearKey(input)
+
     val expected: String = "ASIA (EX. NEAR EAST)"
-
-    val actualResult = Engine.clearKey(input)
-
     assertEquals(actualResult, expected)
-  }
-
-  //  @Ignore
-  @Test
-  def getPopulationDensityAtRegionsTest(): Unit = {
-    val expected: RDD[(String, Double)] = getTestRDD().map {
-      case (x1, x2, x3) => (clearKey(x1), x3)
-    }.reduceByKey(_ + _)
-
-    assertEquals(
-      Engine.getPopulationDensityAtRegions(getTestRDD()).count(),
-      expected.count()
-    )
-  }
-
-  @Test
-  def getPopulationSumTest(): Unit = {
-    val expected: Long = getTestRDD()
-      .map(_._2)
-      .reduce(_ + _)
-
-    assertEquals(
-      Engine.getPopulationSum(getTestRDD()),
-      expected
-    )
   }
 
   @Test
   def getCaseRegionIPopultionIpopDensityTest(): Unit = {
     val input = "Afghanistan ,ASIA (EX. NEAR EAST)         ,31056997,647500,\"48,0\",\"0,00\",\"23,06\",\"163,07\",700,\"36,0\",\"3,2\",\"12,13\",\"0,22\",\"87,65\",1,\"46,6\",\"20,34\",\"0,38\",\"0,24\",\"0,38\""
-    val expected = "(ASIA (EX. NEAR EAST)         ,31056997,48.0)"
-    val testData: (String, Long, Double) = Engine.getCaseRegionIPopultionIpopDensity(input)
 
-    assertEquals(testData._1, "ASIA (EX. NEAR EAST)         ")
-    assertEquals(testData._2, 31056997l)
+    val actualResult: (String, Long, Double) = new Engine() {}.getCaseRegionIPopultionIpopDensity(input)
+
+    val expected = "(ASIA (EX. NEAR EAST)         ,31056997,48.0)"
+    assertEquals(actualResult._1, "ASIA (EX. NEAR EAST)         ")
+    assertEquals(actualResult._2, 31056997l)
     //    assertEquals(testData._3, 48.0)
 
-    assertEquals(
-      testData.toString(),
-      expected
+    assertEquals(actualResult.toString(), expected
     )
   }
 
+  @Test
+  def isHeaderCsv(): Unit = {
+    val input = "ASIA (EX. NEAR EAST)         "
 
-  private def getTestRDD(): RDD[(String, Long, Double)] = {
-    val sparkContext: SparkContext = SparkSession
-      .builder()
-      .appName("SparkSessionZipsExample")
-      .master("local")
-      .getOrCreate().sparkContext
+    val actualResult = new Engine() {}.isHeaderCsv(input)
 
-    val inputFile: RDD[String] = sparkContext
-      .textFile("src\\test\\scala\\resorses\\countries_of_the_world.csv")
-
-    val header: String = inputFile.first()
-
-    val populationIndex: Int = 2
-    val regionIndex: Int = 1
-    val popDensityIntegerIndex: Int = 4
-    val popDensityFractionalIndex: Int = 5
-
-    inputFile.filter(_ != header).map(line => { //ignore of first row that contains column names
-      val colAr: Array[String] = line.split(",") //get array with columns
-      (
-        colAr(regionIndex),
-
-        colAr(populationIndex) //string to long
-          .toLong,
-
-        colAr(popDensityIntegerIndex)
-          .concat("." + colAr(popDensityFractionalIndex)) //e.g.: "528', '8" -> "528.8"
-          .replace("\"", "").toDouble //e.g.: "528.8" -> 528.8
-      )
-    })
+    assertEquals(actualResult, false)
   }
+
+
 }
